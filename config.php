61d707bc71532ff7998a498ad8cb66de75fe1aff
XianyeZ <35696186+XianyeZ@users.noreply.github.com>
2020-05-11 15:49:26 -0500

    update eecs sql server info
    including servername, username, password and db names

--- config.php
+++ config.php
@@ -8,4 +8,14 @@
 	if(mysqli_connect_errno()){
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}
+// $servername = "mysql.eecs.ku.edu"
+// $username = "x534z011"
+// $password = "jee4Aey9"
+// db names:
+// Contains: Contains
+// Ingredient: Ingredient
+// Recipe: Recipe
+// Upload: Upload
+// User: User
+// UserRestriction: UserRistriction
 ?>
