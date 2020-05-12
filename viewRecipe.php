5035d1ac39650b1a5371628b42df4137946d13a9
XZhou <x534z011@ku.edu>
2020-05-11 22:05:20 -0500

    update to view recipe

--- viewRecipe.php
+++ viewRecipe.php
@@ -28,6 +28,13 @@ if($stmt = $conn->prepare($sql)){
     if($stmt = $conn->prepare($sql)){
         $stmt->bind_param("i", $rid);
         $result = $stmt->execute();
+
+        if($result->num_rows > 0){
+            echo "<h2>Ingredients        Quantity</h2><br>";
+            while($row = $result->fetch_assoc()){
+                echo "".$row["IngName"]."    ".$row["Amount"]."<br>";
+            }
+        }
     }
     ?>
 
